39d9c7d6c610601506a0170c5689bbb1a886797f
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-09-27 16:30:08 +0300

    fix linter errors

--- Person.rb
+++ Person.rb
@@ -2,26 +2,27 @@ class Person
     attr_accessor :name
     attr_accessor :age
     attr_reader :id
-    def initialize(name = 'unknown',age, parent_permission = true)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
+
+    def initialize(name = 'unknown', age, parent_permission: true)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
     end
 
-    private
+        private
+
     def is_of_age?
-        return @age > 18 ? true : false
+    return @age > 18 ? true : false
     end
 
-    public
+        public
+
     def can_use_services?
-        if is_of_age?
-            true
-        elsif parent_permission
-            true
-        else
-            false
-        end
+    if is_of_age?
+        true
+    elsif parent_permission
+        true
+    else
+        false
     end
-
-    
+end 
--- Teacher.rb
+++ Teacher.rb
@@ -1,12 +1,12 @@
-require './Person.rb'
+require './Person'
 
 class Teacher < Person
-    def initialize(name, age, specialization, parent_permission= true)
-        super(name, age, parent_permission)
-        @specialization = specialization
-    end
+  def initialize(name, age, specialization, parent_permission: true)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        return true
-    end
+  def can_use_services?
+    true
+  end
 end
