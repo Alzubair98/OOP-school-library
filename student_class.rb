95b870b7637715ceecf6637e6f0046900148ea91
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-09-28 15:10:35 +0300

    add classes and fix linter errors

--- student_class.rb
+++ student_class.rb
@@ -1,6 +1,7 @@
 require './person'
 
 class Student < Person
+  # attr_accessor :classroo
   def initialize(classroom, age, name, parent_permission)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -8,5 +9,10 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 end
