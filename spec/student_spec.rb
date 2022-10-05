70c8941c0d92ff2af4eb74622c8d3294da4261b3
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-10-05 23:10:17 +0300

    Add a test to a student class

---
+++ spec/student_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../student_class'
+
+describe Student do
+  context 'check if a student class is created' do
+    it 'creates a student' do
+      student = Student.new('CR1', 18, 'Abraha', true)
+      classroom = student.classroom
+      age = student.age
+      name = student.name
+      parent_permission = student.parent_permission
+      expect(classroom).to eq('CR1')
+      expect(age).to eq(18)
+      expect(name).to eq('Abraha')
+      expect(parent_permission).to be true
+    end
+  end
+end
