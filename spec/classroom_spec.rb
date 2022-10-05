d6fabd75e45908d5b934e0d348d6d81a72a9e48c
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-05 22:25:18 +0300

    add 2 tests

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../classroom.rb'
+
+describe Classroom do
+    context "check classroom class" do
+        it "create new classroom" do
+            classroom = Classroom.new('1')
+            label = classroom.label
+            expect(label).to eq('1')
+        end
+    end
+end
